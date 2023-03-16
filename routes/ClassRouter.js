27329834063cbf6d9505a64743e177e0f9ae6195
Tina Yang <Tinayangers@gmail.com>
2023-03-16 18:33:35 -0400

    Added students by class route/controller

--- routes/ClassRouter.js
+++ routes/ClassRouter.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/ClassController')
 Router.post('/', controller.CreateClass)
 Router.get('/get-classes', controller.GetClasses)
 Router.get('/get-classes/:id', controller.GetClassById)
+Router.get('/get-classes/:id/students', controller.GetStudentsByClass)
 
 Router.delete('/delete-class', controller.DeleteClass)
 Router.post('/update-class', controller.UpdateClass)
